453b72e56f9c9f6a43329cbcf0480e8bd303b3a4
Mauricio Robayo <rfmajo@gmail.com>
2021-08-16 10:40:56 -0500

    feat: add Email component
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,55 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react';
+import React, {
+  AnchorHTMLAttributes,
+  DetailedHTMLProps,
+  ReactNode,
+  useState,
+} from 'react';
+import { percentEncodeParams } from './utils';
 
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-  /** custom content, defaults to 'the snozzberries taste like snozzberries' */
-  children?: ReactChild;
-}
-
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: https://github.com/storybookjs/storybook/issues/9556
-/**
- * A custom Thing component. Neat!
- */
-export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+export type Props = DetailedHTMLProps<
+  AnchorHTMLAttributes<HTMLAnchorElement>,
+  HTMLAnchorElement
+> & {
+  body?: string;
+  children?: ReactNode;
+  className?: string;
+  email: string;
+  subject?: string;
 };
+export function Email({
+  body = '',
+  subject = '',
+  children,
+  className,
+  email,
+  ...rest
+}: Props) {
+  const [hovered, setHovered] = useState(false);
+  const emailUrl = new URL(`mailto:${email}`);
+
+  // https://github.com/whatwg/url/issues/18#issuecomment-369865339
+  emailUrl.search = percentEncodeParams({ body, subject });
+
+  function handleHover() {
+    setHovered(true);
+  }
+
+  return (
+    <a
+      className={className}
+      href={hovered ? emailUrl.href : '#'}
+      onFocus={handleHover}
+      onMouseOver={handleHover}
+      {...rest}
+    >
+      {hovered
+        ? children || email
+        : children ||
+          email
+            .split('@')
+            .map((emailSegment) => (
+              <span key={emailSegment}>{emailSegment}</span>
+            ))}
+    </a>
+  );
+}

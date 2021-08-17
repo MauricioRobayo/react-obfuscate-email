40500a58d83faac09415c2ecbaf39eda7921390b
Mauricio Robayo <rfmajo@gmail.com>
2021-08-17 04:55:31 -0500

    test: full test coverage
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- test/Email.test.tsx
+++ test/Email.test.tsx
@@ -4,129 +4,46 @@ import '@testing-library/jest-dom';
 import { Email } from '../src/index';
 
 const fakeEmail = 'test@example.com';
+const displayText = 'Email me!';
 
-describe('obfuscate email until user focuses on it', () => {
-  it('uses email as text content', () => {
-    const { queryByText, getByRole } = render(
-      <Email
-        email={fakeEmail}
-        title="Email me!"
-        target="_blank"
-        rel="noopener noreferrer"
-      />
-    );
-    const link = getByRole('link');
+const events = [
+  { name: 'focus', trigger: fireEvent.focus },
+  { name: 'mouseOver', trigger: fireEvent.mouseOver },
+];
 
-    expect(link).not.toHaveAttribute(
-      'href',
-      expect.stringContaining(fakeEmail)
-    );
-    expect(link).toHaveAttribute('target', '_blank');
-    expect(link).toHaveAttribute('rel', 'noopener noreferrer');
-    expect(link).toHaveAttribute('title', 'Email me!');
-    expect(queryByText(fakeEmail)).not.toBeInTheDocument();
+const cases = [
+  {
+    component: <Email email={fakeEmail} />,
+    text: fakeEmail,
+    obfuscatedText: fakeEmail.replace('@', ''),
+  },
+  {
+    component: <Email email={fakeEmail}>{displayText}</Email>,
+    text: displayText,
+    obfuscatedText: displayText,
+  },
+];
 
-    fireEvent.focus(link);
+describe.each(events)("obfuscate email until $name", ({ trigger }) => {
+  it.each(cases)(
+    "renders $obfuscatedText",
+    ({ component, text, obfuscatedText }) => {
+      const { queryByText, getByRole, getByText } = render(component);
+      const link = getByRole('link');
 
-    expect(link).toHaveAttribute('href', `mailto:${fakeEmail}`);
-    expect(link).toHaveAttribute('target', '_blank');
-    expect(link).toHaveAttribute('rel', 'noopener noreferrer');
-    expect(link).toHaveAttribute('title', 'Email me!');
-    expect(queryByText(fakeEmail)).toBeInTheDocument();
-  });
+      expect(link).not.toHaveAttribute(
+        'href',
+        expect.stringContaining(fakeEmail)
+      );
+      expect(queryByText(fakeEmail)).not.toBeInTheDocument();
+      expect(getByText(obfuscatedText)).toBeInTheDocument();
 
-  it('uses children as text content', () => {
-    const { getByText, getByRole } = render(
-      <Email
-        email={fakeEmail}
-        title="Email me!"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Email me!
-      </Email>
-    );
-    const link = getByRole('link');
+      trigger(link);
 
-    expect(link).not.toHaveAttribute(
-      'href',
-      expect.stringContaining(fakeEmail)
-    );
-    expect(link).toHaveAttribute('target', '_blank');
-    expect(link).toHaveAttribute('rel', 'noopener noreferrer');
-    expect(link).toHaveAttribute('title', 'Email me!');
-    expect(getByText('Email me!')).toBeInTheDocument();
-
-    fireEvent.focus(link);
-
-    expect(link).toHaveAttribute('href', `mailto:${fakeEmail}`);
-    expect(link).toHaveAttribute('target', '_blank');
-    expect(link).toHaveAttribute('rel', 'noopener noreferrer');
-    expect(link).toHaveAttribute('title', 'Email me!');
-    expect(getByText('Email me!')).toBeInTheDocument();
-  });
-});
-
-describe('obfuscates email until user hovers over it', () => {
-  it('uses email as text content', () => {
-    const { queryByText, getByRole } = render(
-      <Email
-        email={fakeEmail}
-        title="Email me!"
-        target="_blank"
-        rel="noopener noreferrer"
-      />
-    );
-    const link = getByRole('link');
-
-    expect(link).not.toHaveAttribute(
-      'href',
-      expect.stringContaining(fakeEmail)
-    );
-    expect(link).toHaveAttribute('target', '_blank');
-    expect(link).toHaveAttribute('rel', 'noopener noreferrer');
-    expect(link).toHaveAttribute('title', 'Email me!');
-    expect(queryByText(fakeEmail)).not.toBeInTheDocument();
-
-    fireEvent.mouseOver(link);
-
-    expect(link).toHaveAttribute('href', `mailto:${fakeEmail}`);
-    expect(link).toHaveAttribute('target', '_blank');
-    expect(link).toHaveAttribute('rel', 'noopener noreferrer');
-    expect(link).toHaveAttribute('title', 'Email me!');
-    expect(queryByText(fakeEmail)).toBeInTheDocument();
-  });
-
-  it('uses children as text content', () => {
-    const { getByText, getByRole } = render(
-      <Email
-        email={fakeEmail}
-        title="Email me!"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Email me!
-      </Email>
-    );
-    const link = getByRole('link');
-
-    expect(link).not.toHaveAttribute(
-      'href',
-      expect.stringContaining(fakeEmail)
-    );
-    expect(link).toHaveAttribute('target', '_blank');
-    expect(link).toHaveAttribute('rel', 'noopener noreferrer');
-    expect(link).toHaveAttribute('title', 'Email me!');
-    expect(getByText('Email me!')).toBeInTheDocument();
-
-    fireEvent.mouseOver(link);
-
-    expect(link).toHaveAttribute('href', `mailto:${fakeEmail}`);
-    expect(link).toHaveAttribute('target', '_blank');
-    expect(link).toHaveAttribute('rel', 'noopener noreferrer');
-    expect(link).toHaveAttribute('title', 'Email me!');
-    expect(getByText('Email me!')).toBeInTheDocument();
-  });
+      expect(link).toHaveAttribute('href', `mailto:${fakeEmail}`);
+      expect(getByText(text)).toBeInTheDocument();
+    }
+  );
 });
 
 describe('properly set query string', () => {

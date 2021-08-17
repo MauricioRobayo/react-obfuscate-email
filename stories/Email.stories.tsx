3cd1e2c92c6c0da74ff74562e865a3034195e849
Mauricio Robayo <rfmajo@gmail.com>
2021-08-17 12:14:20 -0500

    docs: use storybook
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

---
+++ stories/Email.stories.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import { Email } from "../src/Email";
+
+export default {
+  title: "Examples",
+  component: Email,
+  argTypes: {
+    email: { defaultValue: "test@example.com" },
+  },
+} as ComponentMeta<typeof Email>;
+
+const Template: ComponentStory<typeof Email> = (args) => <Email {...args} />;
+
+export const Simple = Template.bind({});
+Simple.args = {};
+
+export const Body = Template.bind({});
+Body.args = {
+  body: "You rock!",
+};
+
+export const Subject = Template.bind({});
+Subject.args = {
+  subject: "Hi 👋",
+};
+
+export const Children = Template.bind({});
+Children.args = {
+  children: "📧 Email me!",
+};
+
+export const Attributes = Template.bind({});
+Attributes.args = {
+  title: "📧 Email me!",
+  rel: "noreferrer noopener",
+  target: "_blank",
+};

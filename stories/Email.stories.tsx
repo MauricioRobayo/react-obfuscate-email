6f3f60f3492d3e9ca61f9c0fbc98274d21ebb897
Mauricio Robayo <rfmajo@gmail.com>
2021-08-18 11:20:03 -0500

    docs(storybook): update stories
    Signed-off-by: Mauricio Robayo <rfmajo@gmail.com>

--- stories/Email.stories.tsx
+++ stories/Email.stories.tsx
@@ -6,8 +6,49 @@ import { Email } from "../src/Email";
 export default {
   title: "React Obfuscate Email",
   component: Email,
+  description: "test description",
   args: {
     email: "test@example.com",
+  },
+  argTypes: {
+    email: {
+      table: {
+        category: "Required",
+      },
+    },
+    subject: {
+      table: {
+        category: "Optional",
+      },
+    },
+    body: {
+      table: {
+        category: "Optional",
+      },
+    },
+    cc: {
+      table: {
+        category: "Optional",
+      },
+    },
+    bcc: {
+      table: {
+        category: "Optional",
+      },
+    },
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: `
+📧🚫🤖 **Simple and lightweight email obfuscator React component**.
+
+View on [GitHub](https://github.com/MauricioRobayo/react-obfuscate-email).
+
+Until you hover or focus on the link, the \`@\` symbol is stripped out and rendered using \`::after\` css pseudo-element, and the \`href\` attribute value is replaced with \`#\`.
+`,
+      },
+    },
   },
 } as ComponentMeta<typeof Email>;
 
@@ -20,9 +61,6 @@ Default.args = {
   cc: ["cc1@example.com", "cc2@example.com"],
   bcc: ["bcc@example.com"],
 };
-
-export const Simple = Template.bind({});
-Simple.args = {};
 
 export const Children = Template.bind({});
 Children.args = {

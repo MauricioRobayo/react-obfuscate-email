9350b8a83f16fff735ee880d24008faf0333637a
Ludwig Meysel <lmeysel@exocad.com>
2022-11-15 12:19:56 +0100

    Update dependencies and implement esm/cjs bundler

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import PluginReact from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import PluginDts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/index.tsx"),
+      name: "React Obfuscate Email",
+      fileName: "react-obfuscate-email",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  plugins: [
+    //
+    // https://github.com/vitejs/vite/tree/main/packages/plugin-react#readme
+    PluginReact({
+      jsxRuntime: "classic",
+    }),
+
+    //
+    // https://github.com/qmhc/vite-plugin-dts#vite-plugin-dts
+    PluginDts({
+      outputDir: "dist/types",
+      skipDiagnostics: true,
+    }),
+  ],
+});
